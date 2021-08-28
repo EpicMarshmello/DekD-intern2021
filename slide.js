d8cd56ed53097e8db8772ac26984261c8241d678
Bio Teeradech <82809098+EpicMarshmello@users.noreply.github.com>
2021-08-28 23:28:47 +0700

    v.2

--- slide.js
+++ slide.js
@@ -2,13 +2,17 @@ window.onload = () => {
   console.log("page loaded");
 };
 
+var checkRadio = document.querySelector('input[name="radio-btn"]:checked');
+
 function checker() {
   for (let i = 1; i < 5; i++) {
     document.getElementById("radioNav" + i).style.background = "none";
   }
-  var checkRadio = document.querySelector('input[name="radio-btn"]:checked');
+  // checkRadio = document.querySelector('input[name="radio-btn"]:checked');
   document.getElementById("radioNav" + checkRadio.value).style.background =
     "pink";
 }
 
 checker();
+
+function changeImg(n) {}
